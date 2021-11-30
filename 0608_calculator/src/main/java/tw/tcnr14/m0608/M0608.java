6d6a30dfe6cd18e9b02ef4d9572376d546a961c3
LIN,JIAN-CHENG <94939073+Devin900904@users.noreply.github.com>
2021-11-30 16:49:31 +0800

    1234

---
+++ 0608_calculator/src/main/java/tw/tcnr14/m0608/M0608.java
@@ -0,0 +1,175 @@
+package tw.tcnr14.m0608;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DecimalFormat;
+
+public class M0608 extends AppCompatActivity {
+
+    private TextView t001,t002;
+
+    private Button btn0,btn1,btn2,btn3,btn4,btn5,btn6,btn7
+            ,btn8,btn9,btn10,btn11,btn12,btn13,btn14,btn15;
+    private EditText num1;
+    private TextView ans;
+    private double nn;
+    private String s;
+    private String keying;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.m0608);
+        setupcomponent();
+
+    }
+
+    private void setupcomponent() {
+        num1 = (EditText) findViewById(R.id.m0608_t001);
+        num1.setText("");
+        ans = (TextView) findViewById(R.id.m0608_t002);
+
+        t002 =(TextView)findViewById(R.id.m0608_t002);
+        btn0 = (Button)findViewById(R.id.m0608_b000);
+        btn1 =(Button)findViewById(R.id.m0608_b001);
+        btn2 =(Button)findViewById(R.id.m0608_b002);
+        btn3 =(Button)findViewById(R.id.m0608_b003);
+        btn4 =(Button)findViewById(R.id.m0608_b004);
+        btn5 =(Button)findViewById(R.id.m0608_b005);
+        btn6 =(Button)findViewById(R.id.m0608_b006);
+        btn7 =(Button)findViewById(R.id.m0608_b007);
+        btn8 =(Button)findViewById(R.id.m0608_b008);
+        btn9 =(Button)findViewById(R.id.m0608_b009);
+        btn10=(Button)findViewById(R.id.m0608_b010);
+        btn11 =(Button)findViewById(R.id.m0608_b011);
+        btn12 =(Button)findViewById(R.id.m0608_b012);
+        btn13 =(Button)findViewById(R.id.m0608_b013);
+        btn14=(Button)findViewById(R.id.m0608_b014);
+        btn15 =(Button)findViewById(R.id.m0608_b015);
+
+
+        btn0.setOnClickListener(numberOnClick);
+        btn1.setOnClickListener(numberOnClick);
+        btn2.setOnClickListener(numberOnClick);
+        btn3.setOnClickListener(numberOnClick);
+        btn4.setOnClickListener(numberOnClick);
+        btn5.setOnClickListener(numberOnClick);
+        btn6.setOnClickListener(numberOnClick);
+        btn7.setOnClickListener(numberOnClick);
+        btn8.setOnClickListener(numberOnClick);
+        btn9.setOnClickListener(numberOnClick);
+        btn10.setOnClickListener(numberOnClick);
+
+        btn11.setOnClickListener(calOnClick);
+        btn12.setOnClickListener(calOnClick);
+        btn13.setOnClickListener(calOnClick);
+        btn14.setOnClickListener(calOnClick);
+
+        btn15.setOnClickListener(equalOnClick);
+    }
+
+
+    private Button.OnClickListener numberOnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String keying =num1.getText().toString();
+            switch (v.getId()){
+                case R.id.m0608_b000:
+                    num1.setText(keying +"0");
+                    break;
+                case R.id.m0608_b001:
+                    num1.setText(keying +"1");
+                    break;
+                case R.id.m0608_b002:
+                    num1.setText(keying +"2");
+                    break;
+                case R.id.m0608_b003:
+                    num1.setText(keying +"3");
+                    break;
+                case R.id.m0608_b004:
+                    num1.setText(keying +"4");
+                    break;
+                case R.id.m0608_b005:
+                    num1.setText(keying +"5");
+                    break;
+                case R.id.m0608_b006:
+                    num1.setText(keying +"6");
+                    break;
+                case R.id.m0608_b007:
+                    num1.setText(keying +"7");
+                    break;
+                case R.id.m0608_b008:
+                    num1.setText(keying +"8");
+                    break;
+                case R.id.m0608_b009:
+                    num1.setText(keying +"9");
+                    break;
+                case R.id.m0608_b010:
+                    num1.setText(keying +".");
+                    break;
+
+
+            }
+
+        }
+    };
+
+    private Button.OnClickListener calOnClick = new Button.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            try {    nn =Double.parseDouble(num1.getText().toString());
+
+            }catch (Exception e){
+                Toast.makeText(getApplicationContext(),getText(R.string.error),Toast.LENGTH_LONG).show();
+            }
+
+            num1.setText("");
+
+            switch (v.getId()){
+                case R.id.m0608_b011:
+                    s="P";
+                    break;
+                case R.id.m0608_b012:
+                    s="M";
+                    break;
+                case R.id.m0608_b013:
+                    s="X";
+                    break;
+                case R.id.m0608_b014:
+                    s="D";
+                    break;
+
+
+            }
+
+        }
+    };
+    private Button.OnClickListener equalOnClick = new Button.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (num1.getText().toString().trim().length() !=0){
+                double n2=Double.parseDouble(num1.getText().toString());
+                DecimalFormat nf =new DecimalFormat("0.0000");
+
+                if(s.equals("P"))ans.setText(nf.format(nn + n2));
+                if(s.equals("M"))ans.setText(nf.format(nn - n2));
+                if(s.equals("X"))ans.setText(nf.format(nn * n2));
+                if(s.equals("D"))ans.setText(nf.format(nn / n2));
+
+               num1.setText(" ");
+                nn = 0;
+            }else{
+                Toast.makeText(getApplicationContext(),getText(R.string.m0608_error),Toast.LENGTH_LONG).show();
+            }
+
+
+
+        }
+    };
+}
